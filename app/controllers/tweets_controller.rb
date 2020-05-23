f5f04a905382c1866582c3db4fc91a8652580c7a
kishiyann <to5hi5_0315@yahoo.co.jp>
2020-05-23 13:31:20 +0900

    投稿

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,35 @@
 class TweetsController < ApplicationController
+  before_action :move_to_index, except: %i[index show search]
 
   def index
-    @tweets = Tweet.all
+    if params[:category_id]
+      @tweets = Tweet.where(category_id: params[:category_id]).order('created_at DESC').page(params[:page]).per(15)
+      render action: 'index'
+    else
+      @tweets = Tweet.includes(:user).order('created_at DESC').page(params[:page]).per(15)
+    end
   end
 
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.create(tweet_params)
+    if @tweet.save
+      redirect_to action: 'index', notice: '投稿できました'
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:title, :category_id, :image, :text).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
 end
